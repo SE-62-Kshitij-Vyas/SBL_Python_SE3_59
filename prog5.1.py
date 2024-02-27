7529da631a8542c9d4450d63fb2e00d44271cf23
SE-62-Kshitij-Vyas <141722244+SE-62-Kshitij-Vyas@users.noreply.github.com>
2024-02-27 09:37:53 +0530

    Delete prog5.1.py

--- prog5.1.py
+++
@@ -1,9 +0,0 @@
-f = open("python1.txt" , "w")
-strWrite = input("Enter the characters: ")
-f.write(strWrite)
-f.close()
-
-f = open("python1.txt" , "r")
-strRead = f.read()
-print("The characters you entered are:", strRead)
-f.close()
